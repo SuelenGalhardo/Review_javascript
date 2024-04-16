4f1228f4e955e0b5b724f78327b07ccf913af3cc
SuelenGalhardo <suelenranucci@gmail.com>
2024-04-16 13:59:47 +0200

    added repetition structure

--- functions.js
+++ functions.js
@@ -1,132 +1,45 @@
-// // CONTROLE DE FLUXO DE APLICAÇAO 
-// // CONTROLFLOW
+// estrutura de repetição
 
-// // //se estiver sujo
-// // console.log('lavar o copo ')
+//FOR
 
-
-// // console.log('servir café')
-
-
-//   // IF ....  ELSE
-
-// //   let temperature = 37.5
-
-// // if ( temperature >= 37) {
-// //   //apenas será executado o bloco de código caso condição seja true
-// // console.log('Febre')
-// // }
-
-// // else {
-// //   console.log('Saudavel')
-// // // apenas será executado o bloco de código caso condição do if seja false
-// // }
-
-// let temperature = 37.4
-// let highTemperature = temperature >= 37.5
-// let mediumTemperature =  temperature <37.5 && temperature >= 37
-
-// if ( highTemperature) {
-//   //apenas será executado o bloco de código caso condição seja true
-// console.log('Febre alta')
+// for(let i = 10; i > 0; i--) {
+// if( i === 5) {
+//   break;
 // }
 
-// else if (mediumTemperature){
-
-//   console.log('febre moderada')
-
-
-// }
-// else {
-//   console.log('Saudavel')
-// }
-  
-// // apenas será executado o bloco de código caso condição do if seja false
-
-
-
-// /*
-
-// let temperature = 36.9
-// let highTemperature = temperature >= 37.5
-// let mediumTemperature = temperature < 37.5 && temperature >= 37
-
-// if(highTemperature) {
-//     console.log('Febre alta')
-// } else if(mediumTemperature) {
-//     console.log('Febre moderada')
-// } else {
-//     console.log('Saudável')
-// }
-// */
-
-//Switch
-
-/*let expression = ''
-
-switch (expression) { // puxa a expressão para o switch
-  case 'a': // confere se o valor da expressão é o correto
-    console.log('a')
-    break // para a execução do switch apenas se verdadeiro
-  case 'b':
-    console.log('b')
-    break
-  default: // caso nenhum valor seja o correto, realizará a 
-					 //instrução dentro de si.
-    console.log('default')
-    break
-}*/
-
-
-// function calculate(number1, operator, number2) {
-
-//   let result = 0;
-
-//   switch (operator) {
-//       case '+':
-//           result = number1 + number2
-//           break
-//       case '-':
-//           result = number1 - number2 
-//           break
-//       case '*':
-//           result = number1 * number2
-//           break
-//       case '/':
-//           result = number1 / number2 
-//           break
-//       default:
-//           console.log('não implementado')
-//           break
-//   }
-
-//   return result
+// console.log(i)
 // }
 
-// //console.log(calculate(4, '+', 8))
-// console.log(calculate(4, '*', 8))
-// console.log(calculate(4, '/', 8))
-// console.log(calculate(4, '', 8))
+//While para definir o momento da parada- enquanto for
 
+// let i = 4805543232;
 
-///**Throw(lançar, disparar ) e Try(tentar)/Catch( pegar )***/  parte do controle de fluxo da aplicação.
+// while(i < 10) {
+//   console.log(i)
 
-//Throw(lançar, disparar ) 
-function sayMyName(name = '') {
-  if (name === '') {
-      throw 'Nome é obrigatório'
-  }
+//   i/=35
 
-  console.log('depois do errro')
+// }
+
+//FOR OF DECLARAÇAO QUE CRIA UM LUUPE
+
+// let name = 'suelen'
+// let names = ['otto', 'antonia', 'jose']
+
+// for(let name of names) {
+//   console.log(name)
+// }
+
+//FOR IN
+
+let person = {
+  name: 'Otto',
+  age: 30, 
+  weight: 30.5
 }
 
+for(let property in person){
 
-
-// try(VAI EXECUTAR A FUNÇAO /catch (EQUANTO CATCH VAI CAPTURAR O ERRO E ATRIBUIR A VARIAVEL)
-try {
-  sayMyName()
-} catch(e) {
-  console.log(e)
+  console.log(property)
+  console.log(person[property])
 }
-
-console.log('após ao try/catch')
